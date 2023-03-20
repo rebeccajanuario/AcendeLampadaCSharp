a649e0007b723d3c6251d41377def8b28fcd9b93
Aluno <Aluno@IFSP-425890-IMG>
2023-03-20 17:19:32 -0300

    Adicionar arquivos de projeto.

---
+++ Exemplo_01/Abajur.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo_01
+{
+    internal class Abajur
+    {
+        private Lampada bocal;
+
+        public Abajur() 
+        {
+            bocal = null;
+        }
+
+        public void encaixaLampada(Lampada lampada) 
+        {
+            bocal = lampada;
+        }
+
+        public void liga()
+        {
+            bocal.acende();
+        }
+    }
+}
---
+++ Exemplo_01/Lampada.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo_01
+{
+    internal class Lampada
+    {
+        private string estadoLampada;
+
+        public Lampada() 
+        {
+            estadoLampada = "Apagado";
+        }
+
+        public void acende()
+        {
+            estadoLampada = "Aceso";
+        }
+        public void apaga()
+        {
+            estadoLampada = "Apagado";
+        }
+
+        public  void mostraEstado()
+        {
+            if(estadoLampada == "Aceso")
+            {
+                Console.WriteLine("Lampada acesa!");
+            }
+            else
+            {
+                Console.WriteLine("Lampada Apagada");
+            }
+        }
+    }
+}
